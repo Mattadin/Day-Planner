6555e2950382dc19bb78da6ddd0a1e5d4f8359de
Mattadin <m4tt4d1n@gmail.com>
2022-03-24 15:40:32 +1100

    save fixed, load errors

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,14 @@
 var timeDisplayEl = $("#currentDay");
 const saveBtn = document.getElementById("saveBtn");
-let textInfo = document.getElementById("nine-am");
-
-saveBtn.addEventListener("click", saveFnc());
+let textInfoNine = document.getElementById("nine-am");
+let textInfoTen = document.getElementById("ten-am");
+let textInfoEleven = document.getElementById("eleven-am");
+let textInfoTwelve = document.getElementById("twelve-pm");
+let textInfoOne = document.getElementById("one-pm");
+let textInfoTwo = document.getElementById("two-pm");
+let textInfoThree = document.getElementById("three-pm");
+let textInfoFour = document.getElementById("four-pm");
+let textInfoFive = document.getElementById("five-pm");
 
 
 function displayTime() {
@@ -120,8 +126,31 @@ function fivePm() {
 }
 
 function saveFnc() {
-    localStorage.setItem("textInfo", textInfo);
+    console.log('saveFnc fired!');
+    localStorage.setItem("textInfoNine", textInfoNine.value);
+    localStorage.setItem("textInfoTen", textInfoTen.value)
+    localStorage.setItem("textInfoEleven", textInfoEleven.value)
+    localStorage.setItem("textInfoTwelve", textInfoTwelve.value)
+    localStorage.setItem("textInfoOne", textInfoOne.value)
+    localStorage.setItem("textInfoTwo", textInfoTwo.value)
+    localStorage.setItem("textInfoThree", textInfoThree.value)
+    localStorage.setItem("textInfoFour", textInfoFour.value)
+    localStorage.setItem("textInfoFive", textInfoFive.value)
 }
+
+function getSaves() {
+    localStorage.getItem("textInfoNine", textInfoNine.value);
+    localStorage.getItem("textInfoTen", textInfoTen.value);
+    localStorage.getItem("textInfoEleven", textInfoEleven.value);
+    localStorage.getItem("textInfoTwelve", textInfoTwelve.value);
+    localStorage.getItem("textInfoOne", textInfoOne.value);
+    localStorage.getItem("textInfoTwo", textInfoTwo.value);
+    localStorage.getItem("textInfoThree", textInfoThree.value);
+    localStorage.getItem("textInfoFour", textInfoFour.value);
+    localStorage.getItem("textInfoFive", textInfoFive.value);
+}
+
+saveBtn.addEventListener("click", saveFnc);
 
 // Hero running
 displayTime();
@@ -137,3 +166,4 @@ twoPm();
 threePm();
 fourPm();
 fivePm();
+getSaves();
